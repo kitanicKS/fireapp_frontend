c216f5d8d67becc41ee63d0afc92d1b1fd3c13a2
Kevin Schewel <kitanic@web.de>
2025-03-02 00:36:42 +0100

    bugfix für ignore von linter fehlern

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
+// ignore: uri_does_not_exist
 import 'constants.dart';
 
 
@@ -46,8 +47,11 @@ class MyHomePageState extends State<MyHomePage> {
   int boxPosition = 0;
   List<dynamic> fetchedData = [];
 
- final String apiUrl = apiurl;
+// ignore: undefined_identifier
+  final String apiUrl = apiurl;
+// ignore: undefined_identifier
   final String apiUsername = apiusername;
+// ignore: undefined_identifier
   final String apiPassword = apipassword;
 
   Future<List<dynamic>> fetchData(String url, String username, String password) async {
