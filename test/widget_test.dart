8abc93a0f54f9b8badd6705ead4256585c225d0d
Kevin Schewel <kitanic@web.de>
2025-03-02 23:48:02 +0100

    add test cases and rename app in title appbar

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,6 +10,49 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fire_app/main.dart';
 
 void main() {
+
+
+  testWidgets('Table and text "Gegenstand" and "Anzahl" are present', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+ // Find the image with the specific asset path.
+    //final imageFinder = find.byWidgetPredicate((widget) =>
+    //  widget is Image && widget.image is AssetImage && (widget.image as AssetImage).assetName == 'assets/hochregal/14.jpg');
+
+    // Verify that the image is present.
+   // expect(imageFinder, findsOneWidget);
+
+    // Tap on the image.
+   // await tester.tap(imageFinder);
+   7/ await tester.pumpAndSettle();
+
+   // Find the image with the specific asset path.
+    //final imageFinder2 = find.byWidgetPredicate((widget) =>
+    //  widget is Image && widget.image is AssetImage && (widget.image as AssetImage).assetName == 'assets/hochregal/32.jpg');
+
+    // Verify that the image is present.
+    //expect(imageFinder2, findsOneWidget);
+
+ final state = tester.state(find.byType(MyHomePage)) as MyHomePageState;
+    state.setState(() {
+      state.boxSectionSelected = true;
+      state.boxSelected = true;
+    });
+
+    // Rebuild the widget with the updated state.
+    await tester.pump();
+
+    // Tap on the image.
+    //await tester.tap(imageFinder2);
+    //await tester.pumpAndSettle();
+
+    // Verify that the text "Gegenstand" and "Anzahl" are present.
+    expect(find.text('Gegenstand'), findsOneWidget);
+    expect(find.text('Anzahl'), findsOneWidget);
+  });
+
+
   testWidgets('Load Data button is present', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
@@ -55,4 +98,13 @@ void main() {
     expect(find.byWidgetPredicate((widget) =>
       widget is Image && widget.image is AssetImage && (widget.image as AssetImage).assetName == 'assets/hochregal/31.jpg'), findsOneWidget);
   });
+
+  testWidgets('Verify correct app name in AppBar', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    // Verify that the AppBar title is 'Fire App'.
+    expect(find.text('Fire App'), findsOneWidget);
+  });
+
 }
